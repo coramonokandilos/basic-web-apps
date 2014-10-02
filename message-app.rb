d94660e71266c3fca87d2b3d14d66dd2ab80c632
Cora Monokandilos <cora.monokandilos@gmail.com>
2014-10-02 23:07:53 +0000

    Made it involve moods as well

--- message-app.rb
+++ message-app.rb
@@ -2,19 +2,47 @@ require "sinatra"     # Load the Sinatra web framework
 
 get("/") do
   html = ""
-
-  html.concat("<h1>Message of The Day</h1>")
-  html.concat("<a href='/message'>See today's message.</a>")
-
+  html.concat("<body style=\"background-color: green\">")
+  html.concat("<h1>This is a message from YOUR MOM</h1>")
+  html.concat("<a href='/message?mood=good'>Good</a></br>")
+  html.concat("<a href='/message?mood=meh'>Meh</a></br>")
+  html.concat("<a href='/message?mood=bad'>Bad</a></br>")
+  html.concat("</body>")
   body(html)
 end
 
 get("/message") do
-  file_contents = File.read("message-of-the-day.txt")
-
-  html = ""
-  html.concat("<h1>Message of the Day</h1>")
-  html.concat("<p>Today's message is: #{file_contents}")
+  mood = params["mood"]
+  
+  if mood == "good"
+    good_contents = File.read("good-messages.txt")
+    html = ""
+    html.concat("<body style=\"background-color: green\">")
+    html.concat("<h1>Message of the Day</h1>")
+    html.concat("<p>Today's message is: </br> #{good_contents}")
+    html.concat("</body>")
+  elsif mood == "meh"
+    meh_contents = File.read("meh-messages.txt")
+    html = ""
+    html.concat("<body style=\"background-color: green\">")
+    html.concat("<h1>Message of the Day</h1>")
+    html.concat("<p>Today's message is: </br> #{meh_contents}")
+    html.concat("</body>")
+  elsif mood == "bad"
+    bad_contents= File.read("bad-messages.txt")
+    html = ""
+    html.concat("<body style=\"background-color: green\">")
+    html.concat("<h1>Message of the Day</h1>")
+    html.concat("<p>Today's message is: </br> #{bad_contents}")
+    html.concat("</body>")
+  else 
+    file_contents = File.read("message-of-the-day.txt")
+    html = ""
+    html.concat("<body style=\"background-color: green\">")
+    html.concat("<h1>Message of the Day</h1>")
+    html.concat("<p>Today's message is: #{file_contents}")
+    html.concat("</body>")
+  end
 
   body(html)
 end
--- wall-app.rb
+++ wall-app.rb
@@ -9,6 +9,7 @@ class Message
   property :id,         Serial
   property :body,       Text,     required: true
   property :created_at, DateTime, required: true
+  property :mood,       Text,   required: true
 end
 
 DataMapper.finalize()
@@ -22,8 +23,18 @@ end
 post("/messages") do
   message_body = params["body"]
   message_time = DateTime.now
-
-  message = Message.create(body: message_body, created_at: message_time)
+  message_mood = params["mood"]
+  mood_message = "Alright"
+  if message_mood == "happy" 
+    mood_message = File.read("good-messages.txt")
+  elsif message_mood == "meh"
+    mood_message = File.read("meh-messages.txt")
+  elsif message_mood == "sad"
+    mood_message = File.read("sad-messages.txt")
+  elsif message_mood == "mad"
+    mood_message = File.read("bad-messages.txt")
+  end
+  message = Message.create(body: message_body, created_at: message_time, mood: mood_message)
 
   if message.saved?
     redirect("/")
